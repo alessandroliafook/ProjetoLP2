1b327c93ea832b3efb8b758d422c28d3850ec277
Lucas Diniz <lucazdinis@gmail.com>
2016-04-28 17:25:40 -0300

    Modificacoes na hierarquia de pessoa/funcionario/paciente

--- src/pessoal/Paciente.java
+++ src/pessoal/Paciente.java
@@ -1,5 +1,73 @@
 package pessoal;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Paciente extends Pessoa {
 
+	private double peso;
+	private String tipoSanguineo;
+	private String sexo;
+	private String genero;
+	private UUID id;
+	
+	public Paciente(String nome, LocalDate dataNascimento, double peso, String tipoSanguineo,
+			String sexo, String genero, UUID id){
+		
+		super(nome, dataNascimento);
+		
+		setPeso(peso);
+		setTipoSanguineo(tipoSanguineo);
+		setSexo(sexo);
+		setGenero(genero);
+		setId(id);
+	}
+	
+	public UUID getID(){
+		return this.id;
+	}
+	
+
+	public double getPeso() {
+		return peso;
+	}
+
+
+
+	public String getTipoSanguineo() {
+		return tipoSanguineo;
+	}
+
+
+	public String getSexo() {
+		return sexo;
+	}
+
+
+	public String getGenero() {
+		return genero;
+	}
+
+
+	private void setId(UUID id){
+		this.id = id;
+	}
+	
+
+	private void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	private void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	private void setTipoSanguineo(String tipoSanguineo) {
+		this.tipoSanguineo = tipoSanguineo;
+	}
+
+	private void setPeso(double peso) {
+		this.peso = peso;
+	}	
+	
 }
--- src/pessoal/Pessoa.java
+++ src/pessoal/Pessoa.java
@@ -2,16 +2,53 @@ package pessoal;
 
 import java.time.LocalDate;
 
-public abstract class Pessoa {
+public abstract class Pessoa implements Comparable<Pessoa>{
 
 	private String nome;
 	private LocalDate dataNascimento;
 	
 	public Pessoa(String nome, LocalDate dataNascimento){
-		this.nome = nome;
-		this.dataNascimento = dataNascimento;
+		setNome(nome);
+		setData(dataNascimento);
+	}
+
+	public String getNome(){
+		return this.nome;
 	}
 	
-	//public abstract String getMatricula();
+	public LocalDate getData(){
+		return this.dataNascimento;
+	}
+	
+	private void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	private void setData(LocalDate dataNascimento){
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public int compareTo(Pessoa outraPessoa) {
+		return this.nome.compareTo(outraPessoa.getNome());
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Pessoa){
+			Pessoa outro = (Paciente) obj;
+			return this.nome.equals(outro.getNome());
+		}
+		return false;
+	}
+	
 	
 }
