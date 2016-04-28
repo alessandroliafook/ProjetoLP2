739213a05eba3a1971583757f590daaf6304bc3e
alessandroliafook <alessandro.liafook@gmail.com>
2016-04-28 20:52:04 -0300

    Javadoc da verificacao

--- src/util/Verificacao.java
+++ src/util/Verificacao.java
@@ -5,26 +5,46 @@ import exceptions.StringInvalida;
 
 public class Verificacao {
 
-	
-	public static void validaNumero(double preco, String parametro) throws NumeroInvalido {
-		if (preco < 0) {
+	/**
+	 * Metodo que verifica se o numero inserido eh menor que zero.
+	 * 
+	 * @param numero
+	 *            - Numero real a ser verificado.
+	 * @param parametro
+	 *            - Nome do parametro que esta sendo verificado.
+	 * @throws NumeroInvalido
+	 *             - Lanca excecao de Numero Invalido acaso o numero seja menor
+	 *             que zero.
+	 */
+	public static void validaNumero(double numero, String parametro) throws NumeroInvalido {
+		if (numero < 0) {
 			String motivo = "eh menor que zero";
 			throw new NumeroInvalido(parametro, motivo);
 		}
 	}
 
-	public static void validaString(String nome, String parametro) throws StringInvalida {
+	/**
+	 * Metodo que verifica se a String inserida eh igual a null ou vazia.
+	 * 
+	 * @param string
+	 *            - String a ser verificada.
+	 * @param parametro
+	 *            - Nome do parametro que esta sendo verificado.
+	 * @throws StringInvalida
+	 *             - Lanca excecao de String Invalida acaso a String seja igual
+	 *             a null ou vazia, personalizando a mensagem para cada caso.
+	 */
+	public static void validaString(String string, String parametro) throws StringInvalida {
 
-		if (nome.equals(null)) {
+		if (string.equals(null)) {
 			String motivo = "igual a null";
 			throw new StringInvalida(parametro, motivo);
 
-		} else if (nome.trim().equals("")) {
+		} else if (string.trim().equals("")) {
 			String motivo = "vazio";
 			throw new StringInvalida(parametro, motivo);
 
 		}
 	}
 
-	
 }
