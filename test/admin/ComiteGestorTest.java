a44e79e83e8854eeb536a452e23b269d17f4e158
alessandroliafook <alessandro.santos@ccc.ufcg.edu.br>
2016-04-27 23:05:33 -0300

    classe de teste para ComiteGestor

---
+++ test/admin/ComiteGestorTest.java
@@ -0,0 +1,14 @@
+package admin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComiteGestorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
