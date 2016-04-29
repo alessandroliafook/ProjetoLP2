8518af7a28236c8a5632f7acfc89af9b79782d28
Lucas <lucazdinis@gmail.com>
2016-04-28 23:44:22 -0300

    Mudei a privacidade de alguns metodos

--- src/pessoal/Pessoa.java
+++ src/pessoal/Pessoa.java
@@ -2,6 +2,7 @@ package pessoal;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import util.*;
 
@@ -10,37 +11,40 @@ public abstract class Pessoa {
 	private String nome;
 	private LocalDate dataNascimento;
 	private final DateTimeFormatter FORMATO_DE_DATA = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-	
+
 	/**
 	 * 
-	 * @param nome - Nome da pessoa
-	 * @param data - Data de nascimento no formato "dd-mm-aaaa"
-	 * @throws DateTimeParseException - Caso a data nao esteja no formato especificado
-	 * @throws StringInvalidaException - Caso nome ou data sejam string vazias ou nulas
+	 * @param nome
+	 *            - Nome da pessoa
+	 * @param data
+	 *            - Data de nascimento no formato "dd-mm-aaaa"
+	 * @throws DateTimeParseException
+	 *             - Caso a data nao esteja no formato especificado
+	 * @throws StringInvalidaException
+	 *             - Caso nome ou data sejam string vazias ou nulas
 	 */
-	public Pessoa(String nome, String data) throws Exception{
-		
+	public Pessoa(String nome, String data) throws Exception {
+
 		Verificacao.validaString(data, "data de nascimento");
 		Verificacao.validaString(nome, "nome da pessoa");
-		
-		setData(LocalDate.parse(data, FORMATO_DE_DATA));
+
 		setNome(nome);
-		setData(dataNascimento);
+		setData(data);
 	}
 
-	public String getNome(){
+	public String getNome() {
 		return this.nome;
 	}
-	
-	public LocalDate getData(){
+
+	public LocalDate getData() {
 		return this.dataNascimento;
 	}
-	
-	private void setNome(String nome){
+
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	private void setData(LocalDate dataNascimento){
-		this.dataNascimento = dataNascimento;
-	}	
+
+	public void setData(String data) throws DateTimeParseException {
+		this.dataNascimento = LocalDate.parse(data, FORMATO_DE_DATA);
+	}
 }
