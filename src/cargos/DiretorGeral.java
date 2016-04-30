294f88c133ca3e0d91f9fa8dc283866e86edf4d9
Lucas <lucazdinis@gmail.com>
2016-04-30 16:18:43 -0300

    Prontuario adicionado e classes mortas removidas

--- src/cargos/DiretorGeral.java
+++
@@ -1,5 +0,0 @@
-package cargos;
-
-public class DiretorGeral implements CargoIF {
-
-}
--- src/prontuario/Prontuario.java
+++ src/prontuario/Prontuario.java
@@ -3,15 +3,19 @@ package prontuario;
 import java.util.List;
 import pessoal.Paciente;
 
-public class Prontuario {
+public class Prontuario implements Comparable<Prontuario> {
 
 	private Paciente paciente;
-	private List<Object> procedimentos;
+	private List<Object> procedimentos; //Para uso futuro
 	
 	public Prontuario(){}
 	
-	
-	
-	
-	
+	public Paciente getPaciente(){
+		return this.paciente;
+	}
+
+	@Override
+	public int compareTo(Prontuario outroProntuario) {
+		return this.paciente.compareTo(outroProntuario.getPaciente());
+	}
 }
