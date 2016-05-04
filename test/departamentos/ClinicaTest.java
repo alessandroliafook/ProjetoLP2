74ac7e7cdfcaac23ad879cb75a7f748241254b4b
Lucas <lucazdinis@gmail.com>
2016-05-03 23:52:22 -0300

    Corrigidos erros nos testes de unidade

--- test/departamentos/ClinicaTest.java
+++ test/departamentos/ClinicaTest.java
@@ -14,10 +14,10 @@ public class ClinicaTest {
 		try {
 			Clinica clinica = new Clinica();
 			assertEquals(0, clinica.getNumeroCadastros());
-			Paciente paciente1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "AB-");
+			int id1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "AB-");
 			assertEquals(1, clinica.getNumeroCadastros());
-			assertEquals("pedro", clinica.getInfoPaciente(paciente1, "Nome"));
-			assertEquals("1978-09-15", clinica.getInfoPaciente(paciente1, "Data"));
+			assertEquals("pedro", clinica.getInfoPaciente(id1, "Nome"));
+			assertEquals("1978-09-15", clinica.getInfoPaciente(id1, "Data"));
 
 		} catch (Exception e) {
 			fail("Nao deveria lancar excecao");
@@ -28,7 +28,7 @@ public class ClinicaTest {
 		 */
 		try {
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("", "15/09/1978", 45.9, "masculino", "masculino", "AB-");
+			int id1 = clinica.cadastraPaciente("", "15/09/1978", 45.9, "masculino", "masculino", "AB-");
 			fail();
 		} catch (Exception e) {
 			assertEquals("Nao foi possivel cadastrar o paciente. Nome do paciente nao pode ser vazio.", e.getMessage());
@@ -36,7 +36,7 @@ public class ClinicaTest {
 
 		try {
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("pedro", "11/28/2000", 45.9, "masculino", "masculino", "AB-");
+			int id1 = clinica.cadastraPaciente("pedro", "11/28/2000", 45.9, "masculino", "masculino", "AB-");
 			fail();
 		} catch (Exception e) {
 			assertEquals("Nao foi possivel cadastrar o paciente. Data invalida.", e.getMessage());
@@ -44,7 +44,7 @@ public class ClinicaTest {
 
 		try {
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("pedro", "15-09-1978", 45.9, "masculino", "masculino", "AB-");
+			int id1 = clinica.cadastraPaciente("pedro", "15-09-1978", 45.9, "masculino", "masculino", "AB-");
 			fail();
 		} catch (Exception e) {
 			assertEquals("Nao foi possivel cadastrar o paciente. Data invalida.", e.getMessage());
@@ -52,7 +52,7 @@ public class ClinicaTest {
 
 		try {
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "X+");
+			int id1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "X+");
 			fail();
 		} catch (Exception e) {
 			assertEquals("Nao foi possivel cadastrar o paciente. Tipo sanguineo invalido.", e.getMessage());
@@ -60,7 +60,7 @@ public class ClinicaTest {
 
 		try {
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "");
+			int id1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "");
 			fail();
 		} catch (Exception e) {
 			assertEquals("Nao foi possivel cadastrar o paciente. Tipo sanguineo invalido.", e.getMessage());
@@ -68,8 +68,8 @@ public class ClinicaTest {
 
 		try {
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
-			Paciente paciente2 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
+			int id1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
+			int id2 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
 			fail();
 		} catch (Exception e) {
 			assertEquals("Nao foi possivel cadastrar o paciente. Paciente ja cadastrado.", e.getMessage());
@@ -81,9 +81,9 @@ public class ClinicaTest {
 		try {
 			Clinica clinica = new Clinica();
 			assertEquals(0, clinica.getNumeroCadastros());
-			Paciente paciente1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
+			int id1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
 			assertEquals(1, clinica.getNumeroCadastros());
-			Paciente paciente2 = clinica.cadastraPaciente("paulo", "21/12/1932", 98.9, "masculino", "masculino", "A+");
+			int id2 = clinica.cadastraPaciente("paulo", "21/12/1932", 98.9, "masculino", "masculino", "A+");
 			assertEquals(2, clinica.getNumeroCadastros());
 		} catch (Exception e) {
 			fail("Nao deveria lancar excecao");
@@ -94,14 +94,14 @@ public class ClinicaTest {
 	public void testGetInfoPaciente() {
 		try {
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
-			assertEquals("pedro", clinica.getInfoPaciente(paciente1, "Nome"));
-			assertEquals("1978-09-15", clinica.getInfoPaciente(paciente1, "Data"));
-			assertEquals("masculino", clinica.getInfoPaciente(paciente1, "Sexo"));
-			assertEquals("masculino", clinica.getInfoPaciente(paciente1, "Genero"));
-			assertEquals("A+", clinica.getInfoPaciente(paciente1, "TipoSanguineo"));
-			assertEquals("45.9", clinica.getInfoPaciente(paciente1, "Peso"));
-			assertEquals("37", clinica.getInfoPaciente(paciente1, "Idade"));
+			int id1 = clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
+			assertEquals("pedro", clinica.getInfoPaciente(id1, "Nome"));
+			assertEquals("1978-09-15", clinica.getInfoPaciente(id1, "Data"));
+			assertEquals("masculino", clinica.getInfoPaciente(id1, "Sexo"));
+			assertEquals("masculino", clinica.getInfoPaciente(id1, "Genero"));
+			assertEquals("A+", clinica.getInfoPaciente(id1, "TipoSanguineo"));
+			assertEquals("45.9", clinica.getInfoPaciente(id1, "Peso"));
+			assertEquals("37", clinica.getInfoPaciente(id1, "Idade"));
 
 		} catch (Exception e) {
 			fail("Nao deveria lancar excecao");
@@ -115,14 +115,14 @@ public class ClinicaTest {
 			final double MAXERROR = 0.01;
 			Clinica clinica = new Clinica();
 			clinica.cadastraPaciente("pedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
-			Paciente fromProntuario = clinica.getProntuario(0);
-			assertEquals("pedro", fromProntuario.getNome());
-			assertEquals("1978-09-15", fromProntuario.getData());
-			assertEquals("masculino", fromProntuario.getSexo());
-			assertEquals("masculino", fromProntuario.getGenero());
-			assertEquals("A+", fromProntuario.getTipoSanguineo());
-			assertEquals(45.9, fromProntuario.getPeso(), MAXERROR);
-			assertEquals(37, fromProntuario.getIdade());
+			int idFromProntuario = clinica.getProntuario(0);
+			assertEquals("pedro", clinica.getInfoPaciente(idFromProntuario, "Nome"));
+			assertEquals("1978-09-15", clinica.getInfoPaciente(idFromProntuario, "Data"));
+			assertEquals("masculino", clinica.getInfoPaciente(idFromProntuario, "Genero"));
+			assertEquals("masculino", clinica.getInfoPaciente(idFromProntuario, "Sexo"));
+			assertEquals("A+", clinica.getInfoPaciente(idFromProntuario, "TipoSanguineo"));
+			assertEquals("45.9", clinica.getInfoPaciente(idFromProntuario, "Peso"));
+			assertEquals("37", clinica.getInfoPaciente(idFromProntuario, "Idade"));
 		
 		} catch (Exception e) {
 			fail("Nao deveria lancar excecao");
@@ -131,16 +131,16 @@ public class ClinicaTest {
 		try {
 			/* Testa a ordenacao dos prontuarios pelo nome do paciente */
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("Caio", "13/09/1980", 90.0, "masculino", "masculino", "A+");
-			Paciente paciente2 = clinica.cadastraPaciente("Bern", "15/09/1978", 59.9, "masculino", "feminino", "A+");
-			Paciente paciente3 = clinica.cadastraPaciente("Zedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
-			Paciente paciente4 = clinica.cadastraPaciente("Ron", "23/11/1991", 76.9, "masculino", "masculino", "A+");
-			assertEquals(paciente2, clinica.getProntuario(0)); // Bern
-			assertEquals(paciente1, clinica.getProntuario(1)); // Caio
-			assertEquals(paciente4, clinica.getProntuario(2)); // Ron
-			assertEquals(paciente3, clinica.getProntuario(3)); // Zedro
-			assertNotEquals(paciente4, clinica.getProntuario(0));
-			assertNotEquals(paciente2, clinica.getProntuario(3));
+			int id1 = clinica.cadastraPaciente("Caio", "13/09/1980", 90.0, "masculino", "masculino", "A+");
+			int id2 = clinica.cadastraPaciente("Bern", "15/09/1978", 59.9, "masculino", "feminino", "A+");
+			int id3 = clinica.cadastraPaciente("Zedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
+			int id4 = clinica.cadastraPaciente("Ron", "23/11/1991", 76.9, "masculino", "masculino", "A+");
+			assertEquals(id2, clinica.getProntuario(0)); // Bern
+			assertEquals(id1, clinica.getProntuario(1)); // Caio
+			assertEquals(id4, clinica.getProntuario(2)); // Ron
+			assertEquals(id3, clinica.getProntuario(3)); // Zedro
+			assertNotEquals(id4, clinica.getProntuario(0));
+			assertNotEquals(id2, clinica.getProntuario(3));
 
 		} catch (Exception e) {
 			fail("Nao deveria lancar excecao");
@@ -156,10 +156,10 @@ public class ClinicaTest {
 
 		try {
 			Clinica clinica = new Clinica();
-			Paciente paciente1 = clinica.cadastraPaciente("Caio", "13/09/1980", 90.0, "masculino", "masculino", "A+");
-			Paciente paciente2 = clinica.cadastraPaciente("Bern", "15/09/1978", 59.9, "masculino", "feminino", "A+");
-			Paciente paciente3 = clinica.cadastraPaciente("Zedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
-			Paciente paciente4 = clinica.cadastraPaciente("Ron", "23/11/1991", 76.9, "masculino", "masculino", "A+");
+			int id1 = clinica.cadastraPaciente("Caio", "13/09/1980", 90.0, "masculino", "masculino", "A+");
+			int id2 = clinica.cadastraPaciente("Bern", "15/09/1978", 59.9, "masculino", "feminino", "A+");
+			int id3 = clinica.cadastraPaciente("Zedro", "15/09/1978", 45.9, "masculino", "masculino", "A+");
+			int id4 = clinica.cadastraPaciente("Ron", "23/11/1991", 76.9, "masculino", "masculino", "A+");
 			clinica.getProntuario(5);
 			fail();
 		} catch (Exception e) {
